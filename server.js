dde852410628062261a0d1f027f0b640dceed73d
julie <maxijewell@gmail.com>
2020-09-10 17:16:42 +0100

    renamed app.js to server.js to see if will work in heroku

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get("/contact", function (req, res){
   res.render("contact");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000.");
 });
